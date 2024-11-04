09a41581dc8067682cadb585431b24b33bd46eb4
Fabiana Coelho de Souza Leão Silveira <fabicoelhodesouzaleao@gmail.com>
2024-11-04 08:34:54 -0300

    Adicionando as bibliotecas de tiaguinho

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h> // Para a função sleep()
+#include <screen.c> //bibliotecas de Tiaguinho
+#include <timer.c>
+#include <keyboard.c>
 
 void exibirTelaInicial() {
     system("clear"); // Limpa o terminal
