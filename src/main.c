c761fbae001428cead8531e6cd9ecc1cf8f50d83
Fabiana Coelho de Souza Leão Silveira <fabicoelhodesouzaleao@gmail.com>
2024-11-04 08:25:02 -0300

    Adicionando a tela de inicio

--- src/main.c
+++ src/main.c
@@ -1,88 +1,30 @@
-/**
- * main.h
- * Created on Aug, 23th 2023
- * Author: Tiago Barros
- * Based on "From C to C++ course - 2002"
-*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> // Para a função sleep()
 
-#include <string.h>
+void exibirTelaInicial() {
+    system("clear"); // Limpa o terminal
 
-#include "screen.h"
-#include "keyboard.h"
-#include "timer.h"
-
-int x = 34, y = 12;
-int incX = 1, incY = 1;
-
-void printHello(int nextX, int nextY)
-{
-    screenSetColor(CYAN, DARKGRAY);
-    screenGotoxy(x, y);
-    printf("           ");
-    x = nextX;
-    y = nextY;
-    screenGotoxy(x, y);
-    printf("Hello World");
-}
-
-void printKey(int ch)
-{
-    screenSetColor(YELLOW, DARKGRAY);
-    screenGotoxy(35, 22);
-    printf("Key code :");
-
-    screenGotoxy(34, 23);
-    printf("            ");
+    // Exibe o título 
+    printf("############################################################\n");
+    printf("#                                                          #\n");
+    printf("#                 BEM-VINDO AO TETRIS MASTER               #\n");
+    printf("#                                                          #\n");
+    printf("############################################################\n");
     
-    if (ch == 27) screenGotoxy(36, 23);
-    else screenGotoxy(39, 23);
-
-    printf("%d ", ch);
-    while (keyhit())
-    {
-        printf("%d ", readch());
-    }
+    printf("\nPrepare-se para uma partida emocionante de TETRIS!\n");
+    printf("Pressione ENTER para começar...\n");
+    
+    // Aguarda o usuário pressionar Enter para continuar
+    getchar();
 }
 
-int main() 
-{
-    static int ch = 0;
-
-    screenInit(1);
-    keyboardInit();
-    timerInit(50);
-
-    printHello(x, y);
-    screenUpdate();
-
-    while (ch != 10) //enter
-    {
-        // Handle user input
-        if (keyhit()) 
-        {
-            ch = readch();
-            printKey(ch);
-            screenUpdate();
-        }
-
-        // Update game state (move elements, verify collision, etc)
-        if (timerTimeOver() == 1)
-        {
-            int newX = x + incX;
-            if (newX >= (MAXX -strlen("Hello World") -1) || newX <= MINX+1) incX = -incX;
-            int newY = y + incY;
-            if (newY >= MAXY-1 || newY <= MINY+1) incY = -incY;
-
-            printKey(ch);
-            printHello(newX, newY);
-
-            screenUpdate();
-        }
-    }
-
-    keyboardDestroy();
-    screenDestroy();
-    timerDestroy();
+int main() {
+    exibirTelaInicial();
+    
+    // Aqui a gente inicia o jogo
+    printf("Iniciando o jogo...\n");
 
     return 0;
 }
+
