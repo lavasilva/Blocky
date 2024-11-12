7167d7aea2656321a27d3aaecd2aff7b63a5670b
Fabiana Coelho de Souza Leão Silveira <fabicoelhodesouzaleao@gmail.com>
2024-11-12 18:07:24 -0300

    Update keyboard.h

--- include/keyboard.h
+++ include/keyboard.h
@@ -1,19 +1,9 @@
-/**
- * keyboard.h
- * Created on Aug, 23th 2023
- * Author: Tiago Barros
- * Based on "From C to C++ course - 2002"
-*/
-
 #ifndef __KEYBOARD_H__
 #define __KEYBOARD_H__
 
 void keyboardInit();
-
 void keyboardDestroy();
-    
-int keyhit();
-
-int readch();
+int keyhit();  // Verifica se uma tecla foi pressionada
+int readch();  // Lê a tecla pressionada
 
 #endif /* __KEYBOARD_H__ */
