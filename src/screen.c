150b801651cada778ba3810e614dd10b7f693d49
Fabiana Coelho de Souza Leão Silveira <fabicoelhodesouzaleao@gmail.com>
2024-11-12 17:57:00 -0300

    Update screen.c

--- src/screen.c
+++ src/screen.c
@@ -1,71 +1,53 @@
-/**
- * screen.c
- * Created on Aug, 23th 2023
- * Author: Tiago Barros
- * Based on "From C to C++ course - 2002"
-*/
-
-
 #include "screen.h"
 
-
-void screenDrawBorders()
-{
+void screenDrawBorders() {
     char hbc = BOX_HLINE;
     char vbc = BOX_VLINE;
-   
+
     screenClear();
     screenBoxEnable();
-   
+    
+    // Desenhar a borda superior
     screenGotoxy(MINX, MINY);
     printf("%c", BOX_UPLEFT);
-
-
-    for (int i=MINX+1; i<MAXX; i++)
-    {
+    for (int i = MINX + 1; i < MAXX; i++) {
         screenGotoxy(i, MINY);
         printf("%c", hbc);
     }
     screenGotoxy(MAXX, MINY);
     printf("%c", BOX_UPRIGHT);
 
-
-    for (int i=MINY+1; i<MAXY; i++)
-    {
+    // Desenhar as laterais
+    for (int i = MINY + 1; i < MAXY; i++) {
         screenGotoxy(MINX, i);
         printf("%c", vbc);
         screenGotoxy(MAXX, i);
         printf("%c", vbc);
     }
 
-
+    // Desenhar a borda inferior
     screenGotoxy(MINX, MAXY);
     printf("%c", BOX_DWNLEFT);
-    for (int i=MINX+1; i<MAXX; i++)
-    {
+    for (int i = MINX + 1; i < MAXX; i++) {
         screenGotoxy(i, MAXY);
         printf("%c", hbc);
     }
     screenGotoxy(MAXX, MAXY);
     printf("%c", BOX_DWNRIGHT);
-
-
+    
     screenBoxDisable();
-   
 }
 
-
-void screenInit(int drawBorders)
-{
+void screenInit(int drawBorders) {
     screenClear();
-    if (drawBorders) screenDrawBorders();
+    if (drawBorders) {
+        screenDrawBorders();
+    }
     screenHomeCursor();
     screenHideCursor();
 }
 
-
-void screenDestroy()
-{
+void screenDestroy() {
     printf("%s[0;39;49m", ESC); // Reset colors
     screenSetNormal();
     screenClear();
@@ -73,36 +55,17 @@ void screenDestroy()
     screenShowCursor();
 }
 
-
-void screenGotoxy(int x, int y)
-{
-    x = ( x<0 ? 0 : x>=MAXX ? MAXX-1 : x);
-    y = ( y<0 ? 0 : y>MAXY ? MAXY : y);
-   
-    printf("%s[f%s[%dB%s[%dC", ESC, ESC, y, ESC, x);
+void screenGotoxy(int x, int y) {
+    x = (x < 0 ? 0 : x >= MAXX ? MAXX - 1 : x);
+    y = (y < 0 ? 0 : y > MAXY ? MAXY : y);
+    printf("%s[%d;%df", ESC, y + 1, x + 1);
 }
 
-
-void screenSetColor( screenColor fg, screenColor bg)
-{
+void screenSetColor(screenColor fg, screenColor bg) {
     char atr[] = "[0;";
-
-
-    if ( fg > LIGHTGRAY )
-    {
+    if (fg > LIGHTGRAY) {
         atr[1] = '1';
         fg -= 8;
     }
-
-
-    printf("%s%s%d;%dm", ESC, atr, fg+30, bg+40);
+    printf("%s%s%d;%dm", ESC, atr, fg + 30, bg + 40);
 }
-
-
-
-
-
-
-
-
-
