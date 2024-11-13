06959f82b9dee51a9da9e8e1b73fe1ab50f16632
Fabiana Coelho de Souza Leão Silveira <fabicoelhodesouzaleao@gmail.com>
2024-11-13 08:44:18 -0300

    Update screen.h

--- include/screen.h
+++ include/screen.h
@@ -1,113 +1,181 @@
-#include "screen.h"
+/**
+ * screen.h
+ * Created on Aug, 23th 2023
+ * Author: Tiago Barros
+ * Based on "From C to C++ course - 2002"
+ * Reference: https://en.wikipedia.org/wiki/ANSI_escape_code
+*/
 
-// Função para desenhar as bordas do jogo com dimensões variáveis
-void screenDrawBorders(int linhas, int colunas) {
-    char hbc = BOX_HLINE;
-    char vbc = BOX_VLINE;
+#ifndef __SCREEN_H__
+#define __SCREEN_H__
 
-    screenClear();
-    screenBoxEnable();
+#include <stdio.h>
 
-    // Habilita o modo de desenho de caixas no terminal
-void screenBoxEnable() {
+// Terminal control sequences
+#define ESC            "\033"
+#define NORMALTEXT     "[0m"
+#define BOLDTEXT       "[1m"
+#define ITALICTEXT     "[3m"
+#define BLINKTEXT      "[5m"
+#define REVERSETEXT    "[7m"
+#define HOMECURSOR     "[f"
+#define SHOWCURSOR     "[?25h"
+#define HIDECURSOR     "[?25l"
+#define CLEARSCREEN    "[2J"
+
+// BOX Drawing - Unix like terminals
+#define BOX_ENABLE     "(0"
+#define BOX_DISABLE    "(B"
+#define BOX_VLINE      0x78
+#define BOX_HLINE      0x71
+#define BOX_UPLEFT     0x6C
+#define BOX_UPRIGHT    0x6B
+#define BOX_DWNLEFT    0x6D
+#define BOX_DWNRIGHT   0x6A
+#define BOX_CROSS      0x6E
+#define BOX_TLEFT      0X74
+#define BOX_TRIGHT     0X75
+#define BOX_TUP        0X77
+#define BOX_TDOWN      0X76
+
+#define BOX_DIAMOND    0x60
+#define BOX_BLOCK      0x61
+#define BOX_DOT        0x7E
+
+// screen constants
+#define SCRSTARTX      3      // Initial and final screen positions for the game
+#define SCRENDX        75     // It means the area that can be drawn 
+#define SCRSTARTY      1
+#define SCRENDY        23
+
+#define MINX           1      // min screen horizontal pos
+#define MINY           1      // min screen vertical pos
+#define MAXX           80     // max screen horizontal pos
+#define MAXY           24     // max screen vertical pos
+
+/**
+ * Screen Colors type
+*/
+typedef enum {BLACK, RED, GREEN, BROWN, BLUE, MAGENTA, CYAN, LIGHTGRAY,
+        DARKGRAY, LIGHTRED, LIGHTGREEN, YELLOW, LIGHTBLUE, 
+        LIGHTMAGENTA, LIGHTCYAN, WHITE} screenColor; 
+
+
+
+/**
+ * Move the cursor to position (0,0)
+*/
+static inline void screenHomeCursor()
+{
+    printf("%s%s", ESC, HOMECURSOR);
+}
+
+/**
+ * Show the cursor
+*/
+static inline void screenShowCursor()
+{
+    printf("%s%s", ESC, SHOWCURSOR);
+}
+
+/**
+ * Hide the cursor
+*/
+static inline void screenHideCursor()
+{
+    printf("%s%s", ESC, HIDECURSOR);
+}
+
+/**
+ * Clear the screen
+*/
+static inline void screenClear()
+{
+    screenHomeCursor();
+    printf("%s%s", ESC, CLEARSCREEN);
+}
+
+/**
+ * Update screen imediatelly 
+*/
+static inline void screenUpdate() {
+    fflush(stdout);
+}
+
+/**
+ * Set screen mode to "normal"
+*/
+static inline void screenSetNormal()
+{
+    printf("%s%s", ESC, NORMALTEXT);
+}
+
+/**
+ * Set screen mode to "bold"
+*/
+static inline void screenSetBold()
+{
+    printf("%s%s", ESC, BOLDTEXT);
+}
+
+/**
+ * Set screen mode to "blink"
+*/
+static inline void screenSetBlink()
+{
+    printf("%s%s", ESC, BLINKTEXT);
+}
+
+/**
+ * Set screen mode to "reverse"
+*/
+static inline void screenSetReverse()
+{
+    printf("%s%s", ESC, REVERSETEXT);
+}
+
+/**
+ * Enable BOX characters in terminal
+*/
+static inline void screenBoxEnable()
+{
     printf("%s%s", ESC, BOX_ENABLE);
 }
 
-// Desabilita o modo de desenho de caixas no terminal
-void screenBoxDisable() {
+/**
+ * Disable BOX characters in terminal
+*/
+static inline void screenBoxDisable()
+{
     printf("%s%s", ESC, BOX_DISABLE);
 }
-    
-    // Desenhar a borda superior
-    screenGotoxy(MINX, MINY);
-    printf("%c", BOX_UPLEFT);
-    for (int i = MINX + 1; i < colunas; i++) {
-        screenGotoxy(i, MINY);
-        printf("%c", hbc);
-    }
-    screenGotoxy(colunas, MINY);
-    printf("%c", BOX_UPRIGHT);
 
-    // Desenhar as laterais
-    for (int i = MINY + 1; i < linhas; i++) {
-        screenGotoxy(MINX, i);
-        printf("%c", vbc);
-        screenGotoxy(colunas, i);
-        printf("%c", vbc);
-    }
+/**
+ * Clear the screen, set cursor to home position
+ * and optionally draw borders on it.
+ * 
+ * @param drawBorders if not zero, draw borders on screen.
+*/
+void screenInit(int drawBorders);
 
-    // Desenhar a borda inferior
-    screenGotoxy(MINX, linhas);
-    printf("%c", BOX_DWNLEFT);
-    for (int i = MINX + 1; i < colunas; i++) {
-        screenGotoxy(i, linhas);
-        printf("%c", hbc);
-    }
-    screenGotoxy(colunas, linhas);
-    printf("%c", BOX_DWNRIGHT);
-    
-    screenBoxDisable();
-}
+/**
+ * Clear the screen and restores to initial state.
+*/
+void screenDestroy();
 
-// Função para desenhar o tabuleiro do jogo
-void screenDrawTabuleiro(int tabuleiro[][COLUNAS], int linhas, int colunas) {
-    for (int y = 0; y < linhas; y++) {
-        for (int x = 0; x < colunas; x++) {
-            screenGotoxy(x + MINX, y + MINY);
-            if (tabuleiro[y][x]) {
-                printf("%c", BOX_BLOCK);
-            } else {
-                printf(" ");
-            }
-        }
-    }
-}
+/**
+ * Move cursor to position (x,y)
+ * @param x x position
+ * @param y y position
+ */ 
 
-// Função para desenhar a peça na posição especificada
-void screenDrawPiece(struct Peca pecaAtual, int x, int y) {
-    for (int i = 0; i < pecaAtual.altura; i++) {
-        for (int j = 0; j < pecaAtual.largura; j++) {
-            if (pecaAtual.forma[i][j]) {
-                screenGotoxy(x + j + MINX, y + i + MINY);
-                printf("%c", BOX_BLOCK);
-            }
-        }
-    }
-}
+void screenGotoxy(int x, int y);
 
-// Função para renderizar a tela (atualiza o buffer do terminal)
-void screenRender() {
-    screenUpdate();
-}
+/**
+ * Define text colors 
+ * @param fg foreground color, can assume values from BLACK to WHITE
+ * @param bg background color, can assume values from BLACK to LIGHTGRAY
+*/
+void screenSetColor(screenColor fg, screenColor bg);
 
-void screenInit(int drawBorders) {
-    screenClear();
-    if (drawBorders) {
-        screenDrawBorders(MAXY, MAXX);
-    }
-    screenHomeCursor();
-    screenHideCursor();
-}
-
-void screenDestroy() {
-    printf("%s[0;39;49m", ESC); // Reset colors
-    screenSetNormal();
-    screenClear();
-    screenHomeCursor();
-    screenShowCursor();
-}
-
-void screenGotoxy(int x, int y) {
-    x = (x < 0 ? 0 : x >= MAXX ? MAXX - 1 : x);
-    y = (y < 0 ? 0 : y > MAXY ? MAXY : y);
-    printf("%s[%d;%df", ESC, y + 1, x + 1);
-}
-
-void screenSetColor(screenColor fg, screenColor bg) {
-    char atr[] = "[0;";
-    if (fg > LIGHTGRAY) {
-        atr[1] = '1';
-        fg -= 8;
-    }
-    printf("%s%s%d;%dm", ESC, atr, fg + 30, bg + 40);
-}
+#endif /* __SCREEN_H__ */
